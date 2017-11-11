ae5e051a479037cffdbd1992b985aa80363c560f
Sheida Mirsayar <Sheida.Mirsayar@gmail.com>
2017-11-11 17:56:05 +0100

    first commit

---
+++ test/main.c
@@ -0,0 +1,43 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 11, 2017
+ *      Author: Sheida
+ */
+
+#include <stdio.h>
+
+
+
+int main(){
+
+
+	printf("hello world!");
+	return 0;
+}
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+void show(int*,int*);
+int main()
+{
+    int b=5;
+    int a=10;
+    printf("main before call :   %d %d\n",a,b);
+  show(&a,&b);
+    printf("main :   %d %d\n",a,b);
+
+    return 0;
+}
+
+void show(int *a,int *b){
+    int x;
+    x=*a;
+    *a=*b;
+    *b=x;
+
+     printf("inside show :   %d %d\n",*a,*b);
+    return;
+}*/
